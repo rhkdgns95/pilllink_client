e34613be60ad2b45162fd1fe4a3017a2c0b28bfe
rhkdgns9489@gmail.com <rhkdgns9489@gmail.com>
2020-01-01 17:02:22 +0900

    # netlify setting - pretty url

--- src/Routes/Login/LoginPresenter.tsx
+++ src/Routes/Login/LoginPresenter.tsx
@@ -181,6 +181,17 @@ const Video = styled.div`
             transform: scale(0);
         }
     }
+    @media(max-width: 510px) {
+        &::after {
+            content: "";
+            position: absolute;
+            top: 70px;
+            left: 0;
+            width: 100%;
+            height: 60px;
+            background-color: black;
+        }
+    }
 `;
 const IFrameMenu = styled.div`
     position: absolute;
